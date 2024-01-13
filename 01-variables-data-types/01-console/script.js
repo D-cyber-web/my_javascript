cfa93051b62736b857dfdbe28649bff7c5195129
D-cyber-web <muthama3david@gmail.com>
2024-01-13 23:31:50 +0300

    first commit

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,20 @@
+console.log(100);
+console.log('Hello world')
+console.log(20, 'hello, true')
+
+const x = 100 
+console.log(x)
+
+console.error('Alert')
+console.warn('Warning')
+
+console.table({name: 'david', email: 'rxample@gamil.com'}) 
+
+console.group('next')
+console.log(x)
+console.error('Alert')
+console.warn('Warning')
+console.groupEnd()
+
+const styles = 'padding: 10px; background-color: black; color: green'
+console.log('%chello world',styles)
