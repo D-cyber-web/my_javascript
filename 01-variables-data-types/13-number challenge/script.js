10588df7a2fd06f74783a8c3ad049a2f71e9800d
D-cyber-web <muthama3david@gmail.com>
2024-01-16 22:48:38 +0300

    update the sandbox

--- 01-variables-data-types/13-number challenge/script.js
+++ 01-variables-data-types/13-number challenge/script.js
@@ -1,60 +1,27 @@
-let x;
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
 
-const name = 'John';
-const age = 31;
+// Get the sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
 
-// Concatenation
-x = 'Hello, my name is ' + name + ' and I am ' + age + ' years old';
+// Get the difference
+const diff = x - y;
+const diffOutput = `${x} - ${y} = ${diff}`;
+console.log(diffOutput);
 
-// Template Literals
-x = `Hello, my name is ${name} and I am ${age} years old`;
+// Get the product
+const prod = x * y;
+const prodOutput = `${x} * ${y} = ${prod}`;
+console.log(prodOutput);
 
-// String Properties and Methods
+// Get the quotient
+const quot = x / y;
+const quotOutput = `${x} / ${y} = ${quot}`;
+console.log(quotOutput);
 
-// Creating a "string object" (JS does this automatically when using a propery or method on a primitive string)
-const s = new String('Hello World');
-
-x = typeof s;
-
-x = s.length;
-
-// Access value by key
-x = s[0];
-
-// Shows the prototype of the string object. Shows the properties and methods
-x = s.__proto__;
-
-// Change case
-x = s.toUpperCase();
-x = s.toLowerCase();
-
-// charAt() - returns the character at the specified index
-x = s.charAt(0);
-
-// indexOf - returns the index of the first occurrence of a specified value in a string
-x = s.indexOf('d');
-
-// substring() - search a string for a specified value
-x = s.substring(2, 5);
-x = s.substring(7);
-
-// slice() - extracts a part of a string and returns a new string
-x = s.slice(-11, -6);
-
-// trim() - remove whitespace from beginning and end of string
-x = '         Hello World';
-x = x.trim();
-
-// replace() - replace all instances of a string
-x = s.replace('World', 'John');
-
-// includes() - returns true if the string is found
-x = s.includes('Hell');
-
-// valueOf() - returns the primitive value of a variable
-x = s.valueOf();
-
-// split() - returns an array of strings
-x = s.split('');
-
-console.log(x);
+// Get the remainder
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+console.log(rmOutput);
