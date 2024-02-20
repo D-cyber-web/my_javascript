cda5923e56bfc58e0ea3cf6fd925713687fa0b10
D-cyber-web <muthama3david@gmail.com>
2024-02-20 20:42:57 +0300

    update the sandbox

--- 06 document object model/08 create item innerHTML vs createElement/shopping list/script.js
+++ 06 document object model/08 create item innerHTML vs createElement/shopping list/script.js
@@ -1,13 +1,32 @@
-const div = document.createElement('div');
-div.className = 'my-element';
-div.id = 'my-element';
-div.setAttribute('title', 'My Element');
-
-// div.innerText = 'Hello World';
-
-const text = document.createTextNode('Hello World');
-div.appendChild(text);
-
-// document.body.appendChild(div);
-
-document.querySelector('ul').appendChild(div);
+// Quick & Dirty
+function createListItem(item) {
+    const li = document.createElement('li');
+  
+    li.innerHTML = `${item}
+    <button class="remove-item btn-link text-red">
+      <i class="fa-solid fa-xmark"></i>
+    </button>`;
+  
+    document.querySelector('.items').appendChild(li);
+  }
+  
+  // Clean & Performant
+  function createNewItem(item) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+  
+    const button = document.createElement('button');
+    button.className = 'remove-item btn-link text-red';
+  
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-xmark';
+  
+    button.appendChild(icon);
+    li.appendChild(button);
+  
+    document.querySelector('.items').appendChild(li);
+  }
+  
+  createListItem('Eggs');
+  createNewItem('Cheese');
+  
