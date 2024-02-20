0ab57d0fd8b1dec83f31284586fdac4d2980ed62
D-cyber-web <muthama3david@gmail.com>
2024-02-20 20:53:12 +0300

    update the sandbox

--- 06 document object model/10 insert elements/shopping list/script.js
+++ 06 document object model/10 insert elements/shopping list/script.js
@@ -1,13 +1,48 @@
-const div = document.createElement('div');
-div.className = 'my-element';
-div.id = 'my-element';
-div.setAttribute('title', 'My Element');
-
-// div.innerText = 'Hello World';
-
-const text = document.createTextNode('Hello World');
-div.appendChild(text);
-
-// document.body.appendChild(div);
-
-document.querySelector('ul').appendChild(div);
+// insertAdjacentElement Example
+function insertElement() {
+    const filter = document.querySelector('.filter');
+  
+    const h1 = document.createElement('h1');
+    h1.textContent = 'insertAdjacentElement';
+  
+    filter.insertAdjacentElement('beforebegin', h1);
+  }
+  
+  // insertAdjacentText Example
+  function insertText() {
+    const item = document.querySelector('li:first-child');
+  
+    item.insertAdjacentText('beforebegin', 'insertAdjacentText');
+  }
+  
+  // insertAdjacentHTML example
+  function insertHTML() {
+    const clearBtn = document.querySelector('#clear');
+  
+    clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
+  }
+  
+  // insertBefore Example
+  function insertBeforeItem() {
+    const ul = document.querySelector('ul');
+  
+    const li = document.createElement('li');
+    li.textContent = 'insertBefore';
+  
+    const thirdItem = document.querySelector('li:nth-child(3)');
+  
+    ul.insertBefore(li, thirdItem);
+  }
+  
+  insertElement();
+  
+  /*
+  <!-- beforebegin -->
+  <p>
+    <!-- afterbegin -->
+    foo
+    <!-- beforeend -->
+  </p>
+  <!-- afterend -->
+  */
+  
