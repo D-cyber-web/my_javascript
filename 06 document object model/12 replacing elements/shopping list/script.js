5db7ab8ce27d331877a2249f9eeef6c1d5424086
D-cyber-web <muthama3david@gmail.com>
2024-02-20 21:27:03 +0300

    update the sandbox

--- 06 document object model/12 replacing elements/shopping list/script.js
+++ 06 document object model/12 replacing elements/shopping list/script.js
@@ -1,48 +1,51 @@
-// insertAdjacentElement Example
-function insertElement() {
-    const filter = document.querySelector('.filter');
-  
-    const h1 = document.createElement('h1');
-    h1.textContent = 'insertAdjacentElement';
-  
-    filter.insertAdjacentElement('beforebegin', h1);
-  }
-  
-  // insertAdjacentText Example
-  function insertText() {
-    const item = document.querySelector('li:first-child');
-  
-    item.insertAdjacentText('beforebegin', 'insertAdjacentText');
-  }
-  
-  // insertAdjacentHTML example
-  function insertHTML() {
-    const clearBtn = document.querySelector('#clear');
-  
-    clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
-  }
-  
-  // insertBefore Example
-  function insertBeforeItem() {
-    const ul = document.querySelector('ul');
-  
-    const li = document.createElement('li');
-    li.textContent = 'insertBefore';
-  
-    const thirdItem = document.querySelector('li:nth-child(3)');
-  
-    ul.insertBefore(li, thirdItem);
-  }
-  
-  insertElement();
-  
-  /*
-  <!-- beforebegin -->
-  <p>
-    <!-- afterbegin -->
-    foo
-    <!-- beforeend -->
-  </p>
-  <!-- afterend -->
-  */
-  
+// replaceWith() Method
+function replaceFirstItem() {
+  const firstItem = document.querySelector('li:first-child');
+
+  const li = document.createElement('li');
+  li.textContent = 'Replaced First';
+
+  firstItem.replaceWith(li);
+}
+
+// OuterHTML Property
+function replaceSecondItem() {
+  const secondItem = document.querySelector('li:nth-child(2)');
+
+  secondItem.outerHTML = '<li>Replaced Second</li>';
+}
+
+// Replace All Items
+function replaceAllItems() {
+  const lis = document.querySelectorAll('li');
+
+  // lis.forEach((item, index) => {
+  //   // item.outerHTML = '<li>Replace All</li>';
+  //   if (index === 1) {
+  //     item.innerHTML = 'Second Item';
+  //   } else {
+  //     item.innerHTML = 'Replace All';
+  //   }
+  // });
+
+  lis.forEach(
+    (item, index) =>
+      (item.outerHTML = index === 1 ? '<li>Second Item</li>' : '<li>Item</li>')
+  );
+}
+
+// replaceChild() Method
+function replaceChildHeading() {
+  const header = document.querySelector('header');
+  const h1 = document.querySelector('header h1');
+
+  const h2 = document.createElement('h2');
+  h2.id = 'app-title';
+  h2.textContent = 'Shopping List';
+  header.replaceChild(h2, h1);
+}
+
+replaceFirstItem();
+replaceSecondItem();
+replaceAllItems();
+replaceChildHeading();
