76116d7d34ba75ec2674db06cacad4008c7eefa3
D-cyber-web <muthama3david@gmail.com>
2024-02-22 00:12:08 +0300

    update the sandbox

--- 07 events/03 event object/shopping list/script.js
+++ 07 events/03 event object/shopping list/script.js
@@ -1,35 +1,50 @@
-const clearBtn = document.querySelector('#clear');
+const logo = document.querySelector('img');
 
-function onClear() {
-  const itemList = document.querySelector('ul');
-  const items = itemList.querySelectorAll('li');
-
-  // itemList.innerHTML = '';
-
-  // items.forEach((item) => item.remove());
-
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild);
-  }
+function onClick(e) {
+  // Event properties
+  // console.log(e.target);
+  // console.log(e.currentTarget);
+  // console.log(e.type);
+  // console.log(e.timeStamp);
+  // console.log(e.clientX);
+  // console.log(e.clientY);
+  // console.log(e.offsetX);
+  // console.log(e.offsetY);
+  // console.log(e.pageX);
+  // console.log(e.pageY);
+  // console.log(e.screenX);
+  // console.log(e.screenY);
 }
 
-// JavaScript Event Listener
-clearBtn.onclick = function () {
-  alert('Clear Items');
-};
+function onDrag(e) {
+  document.querySelector('h1').textContent = `X ${e.clientX} Y ${e.clientY}`;
+}
 
-clearBtn.onclick = function () {
-  console.log('Clear Items');
-};
+logo.addEventListener('click', onClick);
+logo.addEventListener('drag', onDrag);
 
-// addEventListener()
-clearBtn.addEventListener('click', () => alert('Clear Items'));
+// document.body.addEventListener('click', function (e) {
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
 
-// Use named function
-clearBtn.addEventListener('click', onClear);
+// e.preventDefault() method prevents the default behavior
+document.querySelector('a').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('Link was clicked');
+});
 
-// removeEventListener()
-setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
-
-// Fire off event from JS
-setTimeout(() => clearBtn.click(), 5000);
+/*
+- `target` - The element that triggered the event
+- `currentTarget` - The element that the event listener is attached to (These are the same in this case
+- `type` - The type of event that was triggered
+- `timeStamp` - The time that the event was triggered
+- `clientX` - The x position of the mouse click relative to the window
+- `clientY` - The y position of the mouse click relative to the window
+- `offsetX` - The x position of the mouse click relative to the element
+- `offsetY` - The y position of the mouse click relative to the element
+- `pageX` - The x position of the mouse click relative to the page
+- `pageY` - The y position of the mouse click relative to the page
+- `screenX` - The x position of the mouse click relative to the screen
+- `screenY` - The y position of the mouse click relative to the screen
+*/
