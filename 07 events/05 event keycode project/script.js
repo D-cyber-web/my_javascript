6f49da3d68265e6d245ed8d206376c2409806ca6
D-cyber-web <muthama3david@gmail.com>
2024-02-22 00:17:26 +0300

    update the sandbox

--- 07 events/05 event keycode project/script.js
+++ 07 events/05 event keycode project/script.js
@@ -1,35 +1,50 @@
-const clearBtn = document.querySelector('#clear');
+// Method 1
+// window.addEventListener('keydown', (e) => {
+//   const insert = document.getElementById('insert');
 
-function onClear() {
-  const itemList = document.querySelector('ul');
-  const items = itemList.querySelectorAll('li');
+//   insert.innerHTML = `
+//     <div class="key">
+//       ${e.key === ' ' ? 'Space' : e.key}
+//       <small>e.key</small>
+//     </div>
 
-  // itemList.innerHTML = '';
+//     <div class="key">
+//       ${e.keyCode}
+//       <small>e.keyCode</small>
+//     </div>
 
-  // items.forEach((item) => item.remove());
+//     <div class="key">
+//       ${e.code}
+//       <small>e.code</small>
+//     </div>
+//   `;
+// });
 
-  while (itemList.firstChild) {
-    itemList.removeChild(itemList.firstChild);
+// Method 2
+function showKeyCodes(e) {
+  const insert = document.getElementById('insert');
+  insert.innerHTML = '';
+
+  const keyCodes = {
+    'e.key': e.key === ' ' ? 'Space' : e.key,
+    'e.keyCode': e.keyCode,
+    'e.code': e.code,
+  };
+
+  for (let key in keyCodes) {
+    const div = document.createElement('div');
+    div.className = 'key';
+    const small = document.createElement('small');
+
+    const keyText = document.createTextNode(key);
+    const valueText = document.createTextNode(keyCodes[key]);
+
+    small.appendChild(keyText);
+    div.appendChild(valueText);
+    div.appendChild(small);
+
+    insert.appendChild(div);
   }
 }
 
-// JavaScript Event Listener
-clearBtn.onclick = function () {
-  alert('Clear Items');
-};
-
-clearBtn.onclick = function () {
-  console.log('Clear Items');
-};
-
-// addEventListener()
-clearBtn.addEventListener('click', () => alert('Clear Items'));
-
-// Use named function
-clearBtn.addEventListener('click', onClear);
-
-// removeEventListener()
-setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
-
-// Fire off event from JS
-setTimeout(() => clearBtn.click(), 5000);
+window.addEventListener('keydown', showKeyCodes);
